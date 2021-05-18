58936bd3429622ebc4ad14f7924b0a479696a00d
Irested <inxsize@live.fr>
2021-05-18 11:00:34 +0200

    init firebase and auth with github

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+import { AuthProvider } from '../lib/auth';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
